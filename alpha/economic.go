55c43dde47398946f42f4739f9fc71897c952740
Mark Hammond <73718696+markjhammond@users.noreply.github.com>
2022-07-11 15:51:50 -0400

    gofinance-alpha_4 - added createAndSend function for common basic create endpoint and sending for economic data. Added Retails Sales.

--- alpha/economic.go
+++ alpha/economic.go
@@ -11,6 +11,7 @@ const (
 	cpi               string = "CPI"
 	consumerSentiment string = "CONSUMER_SENTIMENT"
 	treasuryYield     string = "TREASURY_YIELD"
+	retailSales       string = "RETAIL_SALES"
 )
 
 type Options struct {
@@ -18,12 +19,12 @@ type Options struct {
 	Maturity Maturity `url:"maturity"`
 }
 
+func (c *Client) RetailSales(opts *Options) (*data.EconomicResponse, error) {
+	return createAndSend(c, retailSales, opts)
+}
+
 func (c *Client) TreasuryYield(opts *Options) (*data.EconomicResponse, error) {
-	endpoint, err := createEndpoint(c.cfg.baseUrl, c.cfg.token, treasuryYield, opts)
-	if err != nil {
-		return nil, err
-	}
-	return get(c, endpoint)
+	return createAndSend(c, treasuryYield, opts)
 }
 
 func (c *Client) Cpi(opts *Options) (*data.EconomicResponse, error) {
@@ -35,19 +36,11 @@ func (c *Client) Cpi(opts *Options) (*data.EconomicResponse, error) {
 	if opts.Interval != Monthly && opts.Interval != SemiAnnual {
 		opts.Interval = Monthly
 	}
-	endpoint, err := createEndpoint(c.cfg.baseUrl, c.cfg.token, cpi, opts)
-	if err != nil {
-		return nil, err
-	}
-	return get(c, endpoint)
+	return createAndSend(c, cpi, opts)
 }
 
 func (c *Client) ConsumerSentiment(opts *Options) (*data.EconomicResponse, error) {
-	endpoint, err := createEndpoint(c.cfg.baseUrl, c.cfg.token, consumerSentiment, opts)
-	if err != nil {
-		return nil, err
-	}
-	return get(c, endpoint)
+	return createAndSend(c, consumerSentiment, opts)
 }
 
 func get(c *Client, endpoint string) (*data.EconomicResponse, error) {
@@ -69,6 +62,14 @@ func get(c *Client, endpoint string) (*data.EconomicResponse, error) {
 	return responseData, nil
 }
 
+func createAndSend(c *Client, function string, opts *Options) (*data.EconomicResponse, error) {
+	endpoint, err := createEndpoint(c.cfg.baseUrl, c.cfg.token, function, opts)
+	if err != nil {
+		return nil, err
+	}
+	return get(c, endpoint)
+}
+
 func createEndpoint(baseUrl string, token string, function string, opts *Options) (string, error) {
 	if opts == nil {
 		opts = &Options{Interval: 0, Maturity: 0}
@@ -87,6 +88,6 @@ func createEndpoint(baseUrl string, token string, function string, opts *Options
 	}
 
 	v, _ := query.Values(&params)
-	
+
 	return fmt.Sprintf("%s?%s", baseUrl, v.Encode()), nil
 }
